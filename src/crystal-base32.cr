5a8e0e1c890a4d168ee35cb07bf4dd3b7800c316
Kingsley Hendrickse <kingsley.hendrickse@gmail.com>
2018-05-29 13:44:35 +0100

    improve perf

--- src/crystal-base32.cr
+++ src/crystal-base32.cr
@@ -1,39 +1,65 @@
 require "./crystal-base32/*"
-require "big"
 
 # Module for encoding and decoding in Base32 as per RFC 3548
-class Base32
+module Base32
+  extend self
   TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
   @@table = TABLE
 
-  def self.table
+  def table
     @@table
   end
 
-  def self.chunks(str, size)
-    result = [] of Chunk
-    bytes = str.bytes
-    while bytes.any?
-      result << Chunk.new(bytes.first(size))
-      bytes = bytes.skip(size)
+  def table_map
+    Hash.zip(@@table.chars, (0..31).to_a)
+  end
+
+  def encode(str : String) : String
+    mio = IO::Memory.new((str.bytesize / 5 * 8))
+    str.to_slice.each_slice(5) do |slice|
+      bits = 0.to(slice.size - 1).reduce(0_u64) { |acc, i| acc | (slice[i].to_u64 << (4 - i)*8) }
+      7.to(0).reduce(0x1F_u64 << 35) do |acc, i|
+        mio << table[((bits & acc) >> i*5) % 32]
+        acc >> 5
+      end
     end
-    result
+
+    # remove trailing chars
+    while mio.buffer[mio.pos - 1] == table[0 % 32].ord
+      mio.pos -= 1
+    end
+
+    # apply padding
+    until (mio.pos % 8 == 0)
+      mio << "="
+    end
+
+    mio.to_s
   end
 
-  def self.decode(str) : String
-    chunks(str, 8).map(&.decode).flatten.join
+  def decode(str : String) : String
+    String.new(decode_as_bytes(str))
   end
 
-  def self.decode_as_bytes(str) : Bytes
-    r = chunks(str, 8).map(&.decode_as_bytes).flatten
-    r.to_unsafe.to_slice(r.size)
+  def decode_as_bytes(str : String) : Bytes
+    mio = IO::Memory.new((str.bytesize / 8) * 5)
+    str.to_slice.reject { |s| ['\n'.ord, '\r'.ord, '='.ord].includes?(s) }.each_slice(8) do |slice|
+      bits = 0.to(slice.size - 1).reduce(0_u64) { |acc, i| acc | table_map[slice[i].chr].to_u64 << (7 - i)*5 }
+      4.to(0).reduce(0xFF_u64 << 32) do |acc, i|
+        mio.write_byte(((bits & acc) >> i*8).to_u8)
+        acc >> 8
+      end
+    end
+
+    # remove trailing zero bytes
+    sl = mio.to_slice
+    while sl[-1] == 0
+      sl = sl[0, sl.size - 1]
+    end if sl.size > 0
+    sl
   end
 
-  def self.encode(str)
-    chunks(str, 5).map(&.encode).flatten.join
-  end
-
-  def self.random_base32(length = 16, padding = true) : String
+  def random_base32(length = 16, padding = true) : String
     random = ""
     Random::Secure.random_bytes(length).each do |b|
       random += self.table[b % 32].to_s
@@ -41,50 +67,12 @@ class Base32
     padding ? random.ljust(((length / 8.0).ceil * 8).to_i32, '=') : random
   end
 
-  def self.table=(table)
+  def table=(table)
     raise Exception.new("Table must have 32 unique characters") unless self.table_valid?(table)
     @@table = table
   end
 
-  def self.table_valid?(table)
+  def table_valid?(table)
     table.bytes.to_a.size == 32 && table.bytes.to_a.uniq.size == 32
-  end
-end
-
-class Chunk
-  def initialize(@bytes : Array(UInt8))
-  end
-
-  def decode
-    n, c = internal_decode
-    (0..n - 1).to_a.reverse.map { |i| ((c >> i * 8) & 0xff).to_i32.chr }
-  end
-
-  def decode_as_bytes
-    n, c = internal_decode
-    (0..n - 1).to_a.reverse.map { |i| ((c >> i * 8) & 0xff).to_u8 }
-  end
-
-  def encode
-    n = (@bytes.size * 8.0 / 5.0).ceil.to_i64
-    p = n < 8 ? 5 - (@bytes.size * 8) % 5 : 0
-    c = @bytes.reduce(0) do |m, o|
-      m = BigInt.new(m)
-      (m << 8) + o
-    end << p
-    [(0..n - 1).to_a.reverse.map { |i| Base32.table[((c >> i * 5) & 0x1f)].to_s }, ("=" * (8 - n))]
-  end
-
-  private def internal_decode
-    bytes = @bytes.take_while { |c| c != 61 } # strip padding
-    n = (bytes.size * 5.0 / 8.0).floor.to_i64
-    p = bytes.size < 8 ? 5 - (n * 8) % 5 : 0
-    c = bytes.reduce(0) do |m, o|
-      m = BigInt.new(m)
-      i = Base32.table.index(o.chr)
-      raise Exception.new("invalid character '#{o.chr}'") if i.nil?
-      (m << 5) + i
-    end >> p
-    {n, c}
   end
 end
--- src/crystal-base32/version.cr
+++ src/crystal-base32/version.cr
@@ -1,3 +1,3 @@
 module Crystal::Base32
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
